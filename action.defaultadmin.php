5bb9e94b650f660f7bcfa438e6b9af230e223855
Yuri <wdwp@ya.ru>
2021-11-28 17:54:04 +0300

    A legacy code is removed

--- action.defaultadmin.php
+++ action.defaultadmin.php
@@ -3,87 +3,83 @@
 if (!isset($gCms)) exit;
 
 if ($this->GetPreference('first_run')) {
-	echo '<p style="color:#F57900;"><img src="../modules/MysqlDump2/icons/emblem-important.png" alt="" width="32" height="32" style="margin-bottom:-8px;margin-right:0.5em;" />'.$this->Lang('first_run').'</p>';
+	echo '<p style="color:#F57900;"><img src="../modules/MysqlDump2/icons/emblem-important.png" alt="" width="32" height="32" style="margin-bottom:-8px;margin-right:0.5em;" />' . $this->Lang('first_run') . '</p>';
 	$params['active_tab'] = 'options';
 }
 echo $this->StartTabHeaders();
-	$tab = (empty($params['active_tab']))?'':$params['active_tab'];
-	echo $this->SetTabHeader('dump', $this->Lang('FORM_Backup_Database'), ($tab == 'dump'));
-	echo $this->SetTabHeader('restore', $this->Lang('FORM_Restore_Database'), ($tab == 'restore'));
-	echo $this->SetTabHeader('delete', $this->Lang('FORM_Delete_Dataset'), ($tab == 'delete'));
-	echo $this->SetTabHeader('options', $this->Lang('FORM_options'), ($tab == 'options'));
+$tab = (empty($params['active_tab'])) ? '' : $params['active_tab'];
+echo $this->SetTabHeader('dump', $this->Lang('FORM_Backup_Database'), ($tab == 'dump'));
+echo $this->SetTabHeader('restore', $this->Lang('FORM_Restore_Database'), ($tab == 'restore'));
+echo $this->SetTabHeader('delete', $this->Lang('FORM_Delete_Dataset'), ($tab == 'delete'));
+echo $this->SetTabHeader('options', $this->Lang('FORM_options'), ($tab == 'options'));
 echo $this->EndTabHeaders();
 
 echo $this->StartTabContent();
 
-	echo $this->StartTab('dump');
-		$this->smarty->assign('formstart', $this->CreateFormStart($id, 'dumpdatabase', $returnid));
-			$hidden = $this->CreateInputHidden($id, 'path', $this->GetPreference('pathtomysqldump'));
-			$hidden .= $this->CreateInputHidden($id, 'options', $this->GetPreference('mysqldumpoptions'));
-			$this->smarty->assign('FORM_hidden', $hidden);
-			$this->smarty->assign('FORM_title', $this->Lang('FORM_title'));
-			$this->smarty->assign('FORM_filename', $this->Lang('FORM_filename'));
-			$this->smarty->assign('FORM_input_filename', $this->CreateInputText($id, 'filename', 'backup-'.date('Y-m-d_H-i-s') . '.sql', 35));
-			$this->smarty->assign('submit', $this->CreateInputSubmit($id, 'backup', $this->Lang('FORM_Backup_Database')));
-		$this->smarty->assign('formend', $this->CreateFormEnd());
-		echo $this->ProcessTemplate('DumpForm.tpl');
-	echo $this->EndTab();
+echo $this->StartTab('dump');
+$this->smarty->assign('formstart', $this->CreateFormStart($id, 'dumpdatabase', $returnid));
+$hidden = $this->CreateInputHidden($id, 'path', $this->GetPreference('pathtomysqldump'));
+$hidden .= $this->CreateInputHidden($id, 'options', $this->GetPreference('mysqldumpoptions'));
+$this->smarty->assign('FORM_hidden', $hidden);
+$this->smarty->assign('FORM_title', $this->Lang('FORM_title'));
+$this->smarty->assign('FORM_filename', $this->Lang('FORM_filename'));
+$this->smarty->assign('FORM_input_filename', $this->CreateInputText($id, 'filename', 'backup-' . date('Y-m-d_H-i-s') . '.sql', 35));
+$this->smarty->assign('submit', $this->CreateInputSubmit($id, 'backup', $this->Lang('FORM_Backup_Database')));
+$this->smarty->assign('formend', $this->CreateFormEnd());
+echo $this->ProcessTemplate('DumpForm.tpl');
+echo $this->EndTab();
 
-	echo $this->StartTab('restore');
-		$this->smarty->assign('formstart', $this->CreateFormStart($id, 'restoredatabase', $returnid));
-			$hidden = $this->CreateInputHidden($id, 'path', $this->GetPreference('pathtomysqldump'));
-			$hidden .= $this->CreateInputHidden($id, 'options', $this->GetPreference('mysqloptions'));
-			$this->smarty->assign('FORM_hidden', $hidden);
-			$this->smarty->assign('FORM_Restore_Database', $this->Lang('FORM_Restore_Database'));
-			$this->smarty->assign('FORM_Warning_Restore', $this->Lang('FORM_Warning_Restore'));
-			$this->smarty->assign('FORM_filename', $this->Lang('FORM_filename'));
-			$this->smarty->assign('FORM_input_filename', $this->CreateInputDropdown($id, 'filename', $this->ListBackupSets()));
-			$AreYouSure = 'onclick="return confirm(\''.$this->Lang('FORM_Warning_Restore').'\n\n'.$this->Lang('AreYouSure').'\');"' ;
-			$this->smarty->assign('submit', $this->CreateInputSubmit($id, 'restore', $this->Lang('FORM_Restore'), $AreYouSure));
-		$this->smarty->assign('formend', $this->CreateFormEnd());
-		echo $this->ProcessTemplate('RestoreForm.tpl');
-	echo $this->EndTab();
+echo $this->StartTab('restore');
+$this->smarty->assign('formstart', $this->CreateFormStart($id, 'restoredatabase', $returnid));
+$hidden = $this->CreateInputHidden($id, 'path', $this->GetPreference('pathtomysqldump'));
+$hidden .= $this->CreateInputHidden($id, 'options', $this->GetPreference('mysqloptions'));
+$this->smarty->assign('FORM_hidden', $hidden);
+$this->smarty->assign('FORM_Restore_Database', $this->Lang('FORM_Restore_Database'));
+$this->smarty->assign('FORM_Warning_Restore', $this->Lang('FORM_Warning_Restore'));
+$this->smarty->assign('FORM_filename', $this->Lang('FORM_filename'));
+$this->smarty->assign('FORM_input_filename', $this->CreateInputDropdown($id, 'filename', $this->ListBackupSets()));
+$AreYouSure = 'onclick="return confirm(\'' . $this->Lang('FORM_Warning_Restore') . '\n\n' . $this->Lang('AreYouSure') . '\');"';
+$this->smarty->assign('submit', $this->CreateInputSubmit($id, 'restore', $this->Lang('FORM_Restore'), $AreYouSure));
+$this->smarty->assign('formend', $this->CreateFormEnd());
+echo $this->ProcessTemplate('RestoreForm.tpl');
+echo $this->EndTab();
 
-	echo $this->StartTab('delete');
-		$this->smarty->assign('formstart', $this->CreateFormStart($id, 'deletedataset', $returnid));
-			$this->smarty->assign('FORM_Delete_Dataset', $this->Lang('FORM_Delete_Dataset'));
-			$this->smarty->assign('FORM_filename', $this->Lang('FORM_filename'));
-			$files = $this->ListBackupSets();
-			$this->smarty->assign('FORM_input_filename', $this->CreateInputDropdown($id, 'filename', $files));
-			$AreYouSure = 'onclick="return confirm(\''.$this->Lang('FORM_Warning_Delete').'\n\n'.$this->Lang('AreYouSure').'\');"' ;
-			$this->smarty->assign('submit', $this->CreateInputSubmit($id, 'delete', $this->Lang('FORM_Delete'), $AreYouSure));
-		$this->smarty->assign('formend', $this->CreateFormEnd());
-		echo $this->ProcessTemplate('DeleteForm.tpl');
-	echo $this->EndTab();
+echo $this->StartTab('delete');
+$this->smarty->assign('formstart', $this->CreateFormStart($id, 'deletedataset', $returnid));
+$this->smarty->assign('FORM_Delete_Dataset', $this->Lang('FORM_Delete_Dataset'));
+$this->smarty->assign('FORM_filename', $this->Lang('FORM_filename'));
+$files = $this->ListBackupSets();
+$this->smarty->assign('FORM_input_filename', $this->CreateInputDropdown($id, 'filename', $files));
+$AreYouSure = 'onclick="return confirm(\'' . $this->Lang('FORM_Warning_Delete') . '\n\n' . $this->Lang('AreYouSure') . '\');"';
+$this->smarty->assign('submit', $this->CreateInputSubmit($id, 'delete', $this->Lang('FORM_Delete'), $AreYouSure));
+$this->smarty->assign('formend', $this->CreateFormEnd());
+echo $this->ProcessTemplate('DeleteForm.tpl');
+echo $this->EndTab();
 
-	echo $this->StartTab('options');
-		$this->smarty->assign('formstart', $this->CreateFormStart($id, 'saveoptions', $returnid));
-			$this->smarty->assign('FORM_options', $this->Lang('FORM_options'));
-			$this->smarty->assign('FORM_pathtomysqldump', $this->Lang('FORM_pathtomysqldump'));
-			$this->smarty->assign('FORM_pathtomysqldump_help', $this->Lang('FORM_pathtomysqldump_help'));
-			$this->smarty->assign('FORM_input_path', $this->CreateInputText($id, 'path', $this->GetPreference('pathtomysqldump'), 35));
-			$this->smarty->assign('FORM_input_options', $this->CreateInputText($id, 'options', $this->GetPreference('mysqldumpoptions'), 80));
-			$this->smarty->assign('FORM_parameters', $this->Lang('FORM_parameters'));
+echo $this->StartTab('options');
+$this->smarty->assign('formstart', $this->CreateFormStart($id, 'saveoptions', $returnid));
+$this->smarty->assign('FORM_options', $this->Lang('FORM_options'));
+$this->smarty->assign('FORM_pathtomysqldump', $this->Lang('FORM_pathtomysqldump'));
+$this->smarty->assign('FORM_pathtomysqldump_help', $this->Lang('FORM_pathtomysqldump_help'));
+$this->smarty->assign('FORM_input_path', $this->CreateInputText($id, 'path', $this->GetPreference('pathtomysqldump'), 35));
+$this->smarty->assign('FORM_input_options', $this->CreateInputText($id, 'options', $this->GetPreference('mysqldumpoptions'), 80));
+$this->smarty->assign('FORM_parameters', $this->Lang('FORM_parameters'));
 
-			$config = $gCms->GetConfig();
-			if ($config['dbms'] == 'mysql') {
-				$this->smarty->assign('FORM_running', $this->Lang('FORM_running',mysql_get_server_info(),$this->MySQL_Recommend()));
-			} else {								
-				$this->smarty->assign('FORM_running', '');
-			}
+$config = $gCms->GetConfig();
 
-			$this->smarty->assign('FORM_parameters_explained', $this->Lang('FORM_parameters_explained'));
-			$this->smarty->assign('FORM_parameters_help', $this->Lang('FORM_parameters_help'));
-			$this->smarty->assign('FORM_input_restore', $this->CreateInputText($id, 'restore', $this->GetPreference('mysqloptions'), 80));
+$this->smarty->assign('FORM_running', '');
+$this->smarty->assign('FORM_parameters_explained', $this->Lang('FORM_parameters_explained'));
+$this->smarty->assign('FORM_parameters_help', $this->Lang('FORM_parameters_help'));
+$this->smarty->assign('FORM_input_restore', $this->CreateInputText($id, 'restore', $this->GetPreference('mysqloptions'), 80));
 
-			$this->smarty->assign('FORM_tables', $this->Lang('FORM_tables'));
-			$this->smarty->assign('FORM_input_tables', $this->CreateInputRadioGroup($id, 'table_prefix', array($this->Lang('FORM_tables_all')=>false, $this->Lang('FORM_tables_CMSMS_only',$gCms->config['db_prefix'])=>true),$this->GetPreference('tablesoptions')));
+$this->smarty->assign('FORM_tables', $this->Lang('FORM_tables'));
+$this->smarty->assign('FORM_input_tables', $this->CreateInputRadioGroup($id, 'table_prefix', array($this->Lang('FORM_tables_all') => false, $this->Lang('FORM_tables_CMSMS_only', $gCms->config['db_prefix']) => true), $this->GetPreference('tablesoptions')));
 
-			$this->smarty->assign('FORM_restore_parameters', $this->Lang('FORM_restore_parameters'));
-			$this->smarty->assign('FORM_restore_help', $this->Lang('FORM_restore_help'));
-			$this->smarty->assign('FORM_save', $this->CreateInputSubmit($id, 'saveoptions', $this->Lang('FORM_save')));
-		$this->smarty->assign('formend', $this->CreateFormEnd());
-		echo $this->ProcessTemplate('OptionsForm.tpl');
-	echo $this->EndTab();
+$this->smarty->assign('FORM_restore_parameters', $this->Lang('FORM_restore_parameters'));
+$this->smarty->assign('FORM_restore_help', $this->Lang('FORM_restore_help'));
+$this->smarty->assign('FORM_save', $this->CreateInputSubmit($id, 'saveoptions', $this->Lang('FORM_save')));
+$this->smarty->assign('formend', $this->CreateFormEnd());
+echo $this->ProcessTemplate('OptionsForm.tpl');
+echo $this->EndTab();
 
 echo $this->EndTabContent();
--- action.dumpdatabase.php
+++ action.dumpdatabase.php
@@ -6,39 +6,36 @@ if ($this->CheckAccess('Create Mysql Dumps')) {
 
 	$config = $gCms->GetConfig();
 	$d = escapeshellcmd($params['filename']);
-	$output_capture = '../modules/'.$this->GetName().'/dump-verbose.txt';
+	$output_capture = '../modules/' . $this->GetName() . '/dump-verbose.txt';
 	$options = escapeshellcmd($params['options']);
-	$output_file = $this->backupDirectory.'/'.$d;
-	$executable = escapeshellarg($params['path'].'/mysqldump');
+	$output_file = $this->backupDirectory . '/' . $d;
+	$executable = escapeshellarg($params['path'] . '/mysqldump');
 
-	echo '<h2 style="color:#F57900"><img src="../modules/MysqlDump2/icons/emblem-important.png" alt="" width="32" height="32" style="margin-bottom:-8px;margin-right:0.5em;" />'.$this->Lang('DUMP_Please_wait').'</h2><p>'.$this->Lang('DUMP_May_take').'</p>';
+	echo '<h2 style="color:#F57900"><img src="../modules/MysqlDump2/icons/emblem-important.png" alt="" width="32" height="32" style="margin-bottom:-8px;margin-right:0.5em;" />' . $this->Lang('DUMP_Please_wait') . '</h2><p>' . $this->Lang('DUMP_May_take') . '</p>';
 
 	$tables = '';
 	if ($this->GetPreference('tablesoptions')) {
 		$db = cmsms()->GetDb();
-		if ($config['dbms'] == 'mysql') {
-			$tb_prfx = addcslashes(mysql_real_escape_string($config['db_prefix']), "%_"); //escape special characters like  '_'
-		} else {			
-			$tb_prfx = addcslashes($config['db_prefix'], "%_"); //escape special characters like  '_'
-		}
-		$query = 'SHOW TABLES LIKE '.$db->qstr($tb_prfx.'%'); //Get all tables that match prefix
+
+		$tb_prfx = addcslashes($config['db_prefix'], "%_"); //escape special characters like  '_'
+
+		$query = 'SHOW TABLES LIKE ' . $db->qstr($tb_prfx . '%'); //Get all tables that match prefix
 		$result = $db->Execute($query);
 		if (!empty($result)) {
-			$OpSys = strtoupper(substr(PHP_OS, 0,3));
+			$OpSys = strtoupper(substr(PHP_OS, 0, 3));
 			while ($dbr = $result->FetchRow()) {
 				$tabarr = array_values($dbr);
 				switch ($OpSys) { //build list of table names to backup
 					case 'WIN':
-						$tables .= $tabarr[0]." ";
+						$tables .= $tabarr[0] . " ";
 						break;
 					default:
-						$tables .= "'".$tabarr[0]."' ";
+						$tables .= "'" . $tabarr[0] . "' ";
 						break;
 				}
 			}
-		}
-		else {
-			echo '<p style="color:#E41C1C;font-weight:bold;"><img src="../modules/MysqlDump2/icons/dialog-error.png" alt="" width="22" height="22" style="margin-bottom:-8px;margin-right:0.5em;" />'.$this->Lang('DUMP_error_tables').'</p>';
+		} else {
+			echo '<p style="color:#E41C1C;font-weight:bold;"><img src="../modules/MysqlDump2/icons/dialog-error.png" alt="" width="22" height="22" style="margin-bottom:-8px;margin-right:0.5em;" />' . $this->Lang('DUMP_error_tables') . '</p>';
 			return;
 		}
 		$result->Close();
@@ -58,15 +55,15 @@ if ($this->CheckAccess('Create Mysql Dumps')) {
 		$output_capture
 	);
 
-	echo '<h2><img src="../modules/MysqlDump2/icons/utilities-terminal.png" alt="" width="22" height="22" style="margin-bottom:-5px;margin-right:0.25em;" />'.$this->Lang('DUMP_Task_started').'</h2>';
+	echo '<h2><img src="../modules/MysqlDump2/icons/utilities-terminal.png" alt="" width="22" height="22" style="margin-bottom:-5px;margin-right:0.25em;" />' . $this->Lang('DUMP_Task_started') . '</h2>';
 
 	if ($this->safe_mode != true) {
 		//displays system command (username and password hidden)
-		$safe_command = preg_replace('/ (--user=|--password=|\-u=|\-p=)([^ ]*)/',' \\1########', $command);
+		$safe_command = preg_replace('/ (--user=|--password=|\-u=|\-p=)([^ ]*)/', ' \\1########', $command);
 		echo $this->Lang('DUMP_Executing', $safe_command);
 	}
 
-	echo '<p><b>'.$this->Lang('Output').'</b></p>';
+	echo '<p><b>' . $this->Lang('Output') . '</b></p>';
 
 	if (file_exists($output_file)) unlink($output_file);
 	//TODO: makefile would be better
@@ -76,21 +73,21 @@ if ($this->CheckAccess('Create Mysql Dumps')) {
 		echo '<pre>';
 		flush();
 		$header = sprintf('[ %s ] prefix=%s >%s', $options, ($this->GetPreference('tablesoptions')) ? $config['db_prefix'] : $this->Lang('DUMP_prefix'), $output_file);
-		system(sprintf('echo -- %s %s %s module  ', PHP_OS, $this->GetName(), $this->GetVersion()).$header);
-		$this->Audit( 0, $this->Lang('friendlyname').' '.$this->GetVersion(), $header);
+		system(sprintf('echo -- %s %s %s module  ', PHP_OS, $this->GetName(), $this->GetVersion()) . $header);
+		$this->Audit(0, $this->Lang('friendlyname') . ' ' . $this->GetVersion(), $header);
 		system($command, $retval);
 		flush();
 		echo '</pre>';
 
-		if (! is_readable($output_capture)) {
-			echo '<p style="color:#E41C1C;font-weight:bold;"><img src="../modules/MysqlDump2/icons/dialog-error.png" alt="" width="22" height="22" style="margin-bottom:-8px;margin-right:0.5em;" />'.$this->Lang('DUMP_not_readable',$output_capture).'</p>';
+		if (!is_readable($output_capture)) {
+			echo '<p style="color:#E41C1C;font-weight:bold;"><img src="../modules/MysqlDump2/icons/dialog-error.png" alt="" width="22" height="22" style="margin-bottom:-8px;margin-right:0.5em;" />' . $this->Lang('DUMP_not_readable', $output_capture) . '</p>';
 		} else {
 			echo '<pre>';
 			readfile($output_capture);
 			echo '</pre>';
 			flush();
 		}
-		echo '<h2 style="color:green"><img src="../modules/MysqlDump2/icons/document-save-as.png" alt="" width="32" height="32" style="margin-bottom:-8px;margin-right:0.5em;" />'.$this->Lang('DUMP_Task_completed').'</h2>';
+		echo '<h2 style="color:green"><img src="../modules/MysqlDump2/icons/document-save-as.png" alt="" width="32" height="32" style="margin-bottom:-8px;margin-right:0.5em;" />' . $this->Lang('DUMP_Task_completed') . '</h2>';
 
 		if (filesize($output_file) > 1023) {
 			echo sprintf('<p><a href="%s"><b>%s</b></a>... %s%s</p>', $output_file, $d, $this->suffix_bytes(filesize($output_file)), $this->Lang('written_to_disk'));
@@ -102,12 +99,11 @@ if ($this->CheckAccess('Create Mysql Dumps')) {
 			unlink($output_file);
 		}
 
-		if (! file_exists($output_file)) {
-			echo '<p style="color:#E41C1C;font-weight:bold;"><img src="../modules/MysqlDump2/icons/dialog-error.png" alt="" width="22" height="22" style="margin-bottom:-8px;margin-right:0.5em;" />'.$this->Lang('Missing', $output_file).'</p>';
+		if (!file_exists($output_file)) {
+			echo '<p style="color:#E41C1C;font-weight:bold;"><img src="../modules/MysqlDump2/icons/dialog-error.png" alt="" width="22" height="22" style="margin-bottom:-8px;margin-right:0.5em;" />' . $this->Lang('Missing', $output_file) . '</p>';
 		}
-
 	} else {
-		echo '<p style="color:#E41C1C;font-weight:bold;"><img src="../modules/MysqlDump2/icons/dialog-error.png" alt="" width="22" height="22" style="margin-bottom:-8px;margin-right:0.5em;" />'.$this->Lang('DUMP_not_writable',$output_file).'</p>';
+		echo '<p style="color:#E41C1C;font-weight:bold;"><img src="../modules/MysqlDump2/icons/dialog-error.png" alt="" width="22" height="22" style="margin-bottom:-8px;margin-right:0.5em;" />' . $this->Lang('DUMP_not_writable', $output_file) . '</p>';
 	}
 
 	$this->ReturnAdmin($id, $params, $returnid);
